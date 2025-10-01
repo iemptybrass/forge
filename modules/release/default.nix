5b08d2df15ae66666bfb34532db5f07b7c2903e9
iemptybrass <iemptybrass@users.noreply.github.com>
2025-10-01 10:15:23 -0400

    automate 2

--- modules/release/default.nix
+++ modules/release/default.nix
@@ -1,20 +1,14 @@
-{ nixpkgs ? null }:
-let
-  crelease = if nixpkgs == null then null else nixpkgs.lib.trivial.release;
-in
 {
   release =
-    releaseStr:
-      let
-        r = if releaseStr == null then crelease else releaseStr;
-        match = builtins.match ''([0-9]+)\.([0-9]+)'' r;
-      in
-        if match == null then
-          r
-        else
-          let
-            major = builtins.fromJSON (builtins.elemAt match 0);
-            minor = builtins.elemAt match 1;
-          in
-            "${builtins.toString (major - 1)}.${minor}";
+    let
+      parts = builtins.split "\\." nixpkgs.lib.trivial.release;
+    in
+      if builtins.length parts < 2 then
+        nixpkgs.lib.trivial.release
+      else
+        let
+          major = builtins.fromJSON (builtins.elemAt parts 0);
+          minor = (builtins.elemAt parts 1);
+        in
+          "${builtins.toString (major - 1)}.${minor}";
 }
